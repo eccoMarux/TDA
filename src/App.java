df0cdde6a5e67b29e7f93405a64550810adf30d4
eccoMarux <mariaelvira.monserratvidal@yahoo.com>
2022-04-17 21:15:26 -0300

    Saludo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo!");
-        System.out.println("Hola Leo <3");
+        System.out.println("Hola Leo!");
+        System.out.println("Hola Jere!");
     }
 }
