cbff08b5c625b70599e14ab68a84a88bb6f87234
LeoBruno400 <103906500+LeoBruno400@users.noreply.github.com>
2022-04-23 00:30:57 -0300

    toString actualizado

--- src/proyectoAvion/Avion.java
+++ src/proyectoAvion/Avion.java
@@ -1,6 +1,7 @@
 package proyectoAvion;
 
 public class Avion {
+
     // Atributos
     private String identificador;
     private String modelo;
@@ -9,24 +10,25 @@ public class Avion {
     private double velocidadPromedio;
 
     // Constructoras
-    public Avion(String elIdentificador, String elModelo, int laCantidadDeAsientos, double loskmRecorridos, double laVelocidadPromedio) {
-     
-             this.identificador = elIdentificador;
-             this.modelo = elModelo;
-             this.cantidadDeAsientos = laCantidadDeAsientos;
-             this.kmRecorridos = loskmRecorridos;
-             this.velocidadPromedio = laVelocidadPromedio;
-     
-         }
+    public Avion(String elIdentificador, String elModelo, int laCantidadDeAsientos, double loskmRecorridos,
+            double laVelocidadPromedio) {
+
+        this.identificador = elIdentificador;
+        this.modelo = elModelo;
+        this.cantidadDeAsientos = laCantidadDeAsientos;
+        this.kmRecorridos = loskmRecorridos;
+        this.velocidadPromedio = laVelocidadPromedio;
+
+    }
 
     // Contructor seteado con valores default.
     public Avion(String elIdentificador) {
-             this.identificador = elIdentificador;
-             this.modelo = "";
-             this.cantidadDeAsientos = 0;
-             this.kmRecorridos = 0;
-             this.velocidadPromedio = 0;
-         }
+        this.identificador = elIdentificador;
+        this.modelo = "";
+        this.cantidadDeAsientos = 0;
+        this.kmRecorridos = 0;
+        this.velocidadPromedio = 0;
+    }
 
     // Observadores
     public String getIdentificador() {
@@ -48,9 +50,21 @@ public class Avion {
     public double getVelocidadPromedio() {
         return this.velocidadPromedio;
     }
-    // Modificadores
 
+    // Modificadores
     public void setKmRecorridos(double losKmRecorridos) {
         this.kmRecorridos = losKmRecorridos;
+    }
+
+    // Propias del tipo
+    public boolean equals(Avion otroId) {
+        return (identificador.equalsIgnoreCase(otroId.getIdentificador()));
+    }
+
+    public String toString() {
+        String cadena = "Modelo: " + this.getModelo() + "\nIdentificador: " + this.getIdentificador()
+                + "\nCantidad de asientos: " + this.getCantidadDeAsientos() + "\nKm recorridos: "
+                + this.getKmRecorridos() + "\nVelocidad Promedio: " + this.getVelocidadPromedio();
+        return cadena;
     }
 }
