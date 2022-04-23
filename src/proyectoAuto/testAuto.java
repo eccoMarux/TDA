789fe2848b1a2880cbacfaa39b4dc799c21f3284
LeoBruno400 <103906500+LeoBruno400@users.noreply.github.com>
2022-04-23 00:52:37 -0300

    print de autos

--- src/proyectoAuto/testAuto.java
+++ src/proyectoAuto/testAuto.java
@@ -1,7 +1,7 @@
 package proyectoAuto;
 import java.util.Scanner;
 public class testAuto {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         //
         Scanner sc = new Scanner(System.in);
         int cantAutos;
@@ -13,8 +13,7 @@ public class testAuto {
     }
 
     public static int verificarLongitudArreglo() {
-        // Verifica si el número ingresado por el usuario es un número válido para la
-        // longitud de arreglo
+        //Verifica si el número ingresado por el usuario es un número válido para la longitud de arreglo
         Scanner sc = new Scanner(System.in);
         int longitud;
         do {
@@ -24,17 +23,14 @@ public class testAuto {
                 System.out.println("El numero ingresado es incorrecto. Por favor, intentelo nuevamente. ");
             }
         } while (longitud <= 0);
-
         return longitud;
     }
 
     public static void cargarArreglo(Auto[] arregloAuto) {
-        // Carga un arreglo de objeto Avion. Se estima que todos los datos ingresados
-        // son correctos.
+        //Carga un arreglo de objeto Avion. Se estima que todos los datos ingresados son correctos.
         Scanner sc = new Scanner(System.in);
         String laPatente, elPropietario, elTelefono, elServicio;
         int i;
-
         for (i = 0; i < arregloAuto.length; i++) {
             System.out.println((i + 1) + "° auto: ");
             System.out.println("Ingrese la patente: ");
@@ -48,11 +44,10 @@ public class testAuto {
             elServicio = elServicio.toLowerCase();
             System.out.println("");
             arregloAuto[i] = new Auto(laPatente, elPropietario, elTelefono, elServicio);
-        }
+        }     
     }
 
     public static void serviciosContrato(Auto[] arregloAuto, int cantAutos) {
-        //Crea arreglos unidimensionales de cada tipo de servicio y muestra por pantalla cada auto segun el servicio.
         int i, j = 0, k = 0, l = 0, longArr;
         longArr = arregloAuto.length;
 
@@ -81,21 +76,20 @@ public class testAuto {
         l = 0;
         System.out.println("Los Autos con servicio full son: ");
         while (autosFull[j] != null && j < cantAutos) {
-            Auto.toString(autosFull[j]);
+            System.out.println(autosFull[j].toString());
             j++;
         }
         System.out.println("");
         System.out.println("Los autos con servicio basica son:");
         while (autosBasica[k] != null && k < cantAutos) {
-            Auto.toString(autosBasica[k]);
+            System.out.println(autosBasica[k].toString());
             k++;
         }
         System.out.println("");
         System.out.println("Los autos con servicio externo:");
         while (autosExterno[l] != null && l < cantAutos) {
-            Auto.toString(autosExterno[l]);
+            System.out.println(autosExterno[l].toString());
             l++;
         }
     }
-
 }
